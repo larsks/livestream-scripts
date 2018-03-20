83545f8d41a87f1c4410a8e23c3c623f5ba49552
Lars Kellogg-Stedman <lars@oddbit.com>
2018-03-20 09:41:38 -0400

    update livestream.sh to use v4l input

--- livestream.sh
+++ livestream.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-# via https://projects.raspberrypi.org/en/projects/infrared-bird-box/12
-raspivid \
-	-awb sun \
-	-ex auto \
-	-co 20 \
-	-br 60 \
-	-o - -t 0 -w 1024 -h 768 -fps 25  -b 4000000 -g 50 -vf |
-ffmpeg -loglevel warning -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - \
-	-vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/bbbp-ydpm-87tb-8pzx
+: ${FFMPEG_LOGLEVEL:=warning}
+: ${FFMPEG_VIDEO_SIZE:=1024x768}
+
+v4l2-ctl \
+	-c brightness=60 \
+	-c contrast=10 \
+	-c vertical_flip=1 \
+	-c white_balance_auto_preset=6
+
+ffmpeg -hide_banner -loglevel $FFMPEG_LOGLEVEL -re \
+	-ar 44100 -ac 2 -acodec pcm_s16le \
+	-f s16le -ac 2 -i /dev/zero \
+	-f v4l2 -input_format h264 -video_size $FFMPEG_VIDEO_SIZE -i /dev/video0 \
+	-vcodec copy -acodec aac -ab 128k -g 50 -strict experimental \
+	-f flv rtmp://a.rtmp.youtube.com/live2/bbbp-ydpm-87tb-8pzx
