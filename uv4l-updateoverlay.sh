110c6661c22fefc7c97a8ad8797d5c4bb20f0d34
Lars Kellogg-Stedman <lars@oddbit.com>
2018-03-20 12:19:31 +0000

    sed command was broken because of %D

--- uv4l-updateoverlay.sh
+++ uv4l-updateoverlay.sh
@@ -6,9 +6,7 @@ file=$(sed -rne '/^text-filename.*=/{s/^text-filename *= *(.*)$/\1/g;p}' /etc/uv
 [[ "$file" == "" ]] && { echo "no configured overlay file"; exit 1; }
 
 # Parse template
-sed -re "s/\\\$time/`date '+%D %H:%M'`/g" </etc/uv4l/text_template.json >${file}
+sed -re "s|\\\$time|`date '+%D %H:%M'`|g" </etc/uv4l/text_template.json >${file}
 
 # Update overlay text
 v4l2-ctl --set-ctrl=text_overlay=1 &>/dev/null
-
-sleep 1
