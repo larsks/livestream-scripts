7693d62e2ecba5a443ad5681653481eb30ebdc49
Lars Kellogg-Stedman <lars@oddbit.com>
2018-03-20 13:43:42 -0400

    add option for setting thread_queue_size

--- livestream.sh
+++ livestream.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 : ${FFMPEG_LOGLEVEL:=warning}
+: ${FFMPEG_THREAD_QUEUE_SIZE:=256}
 
 if [ -z "$YOUTUBE_STREAM_KEY" ]; then
 	echo "ERROR: missing youtube stream key" >&2
@@ -18,6 +19,7 @@ pycam \
 ffmpeg -hide_banner -loglevel $FFMPEG_LOGLEVEL -re \
 	-ar 44100 -ac 2 -acodec pcm_s16le \
 	-f s16le -ac 2 -i /dev/zero \
+	-thread_queue_size $FFMPEG_THREAD_QUEUE_SIZE \
 	-f h264 -i - \
 	-vcodec copy -acodec aac -ab 128k -g 50 -strict experimental \
 	-f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}
