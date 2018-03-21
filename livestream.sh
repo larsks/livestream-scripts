b978089ac30beb2066451cd41256e3c7b70d0d69
Lars Kellogg-Stedman <lars@oddbit.com>
2018-03-20 21:54:46 -0400

    reformatting

--- livestream.sh
+++ livestream.sh
@@ -19,9 +19,10 @@ pycam \
 	${CAMERA_HEIGHT:+--height $CAMERA_HEIGHT} \
 	${CAMERA_RESOLUTION:+--resolution $CAMERA_RESOLUTION} |
 ffmpeg -hide_banner -loglevel $FFMPEG_LOGLEVEL \
-	-ar 44100 -ac 2 -acodec pcm_s16le \
-	-f s16le -ac 2 -i /dev/zero \
-	-thread_queue_size $FFMPEG_THREAD_QUEUE_SIZE \
-	-f h264 -i - \
-	-vcodec copy -acodec aac -ab 128k -g 50 -strict experimental \
-	-f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}
+		-ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 \
+	-i /dev/zero \
+		-thread_queue_size $FFMPEG_THREAD_QUEUE_SIZE -f h264 \
+	-i - \
+		-vcodec copy -acodec aac -ab 128k \
+		-g 50 -strict experimental -f flv \
+	rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}
